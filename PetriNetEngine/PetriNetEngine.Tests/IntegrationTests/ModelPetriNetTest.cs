9d4364cb2c428e3778f0d56a48913a67a1937032
jfriisKU <xdr476@alumni.ku.dk>
2022-06-09 16:57:28 +0200

    Fix test with escape

--- PetriNetEngine/PetriNetEngine.Tests/IntegrationTests/ModelPetriNetTest.cs
+++ PetriNetEngine/PetriNetEngine.Tests/IntegrationTests/ModelPetriNetTest.cs
@@ -5,6 +5,10 @@ using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using SEA_Models.PetriNet;
+
 namespace PetriNetEngine.Tests;
 
 public class ModelPetriNetTest
@@ -26,13 +30,29 @@ public class ModelPetriNetTest
     }
     
     [Fact]
-    public async Task Create_moddeling()
+    public async Task Create_modeling()
     {
         await using var application = new WebApplicationFactory<Program>();
         var client = application.CreateClient();
 
-        var response = await client.PostAsync("api/v1/Model", System.Net.Http.HttpContent);
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        PetriNet p = new PetriNet();
+        p.Places = new List<Place>()
+        {
+            new Place()
+        };
+        p.Transitions = new List<Transition>()
+        {
+            new Transition()
+        };
+        p.Arcs = new List<Arc>()
+        {
+            new Arc()
+        };
+        
+        StringContent httpContent = new StringContent("{    \"name\": \"P\",    \"time\": 0,    \"maxTokenId\": 0,    \"arcs\": [      {        \"sourceNode\": 0,        \"targetNode\": 1,        \"type\": 0      }    ],    \"places\": [      {        \"placeId\": 1,        \"name\": \"Place\",        \"maxAge\": 0,        \"isUrgent\": false,        \"numberOfTokens\": 2147483647,        \"tokens\": [          {            \"id\": 0,            \"tokenId\": 0,            \"name\": \"string\",            \"age\": 0          }        ]      }    ],    \"transitions\": [      {        \"transitionId\": 0,        \"name\": \"0\"      }    ]  }", System.Text.Encoding.UTF8, "application/json");
+
+        var response = await client.PostAsync("api/v1/Model", httpContent);
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         
     }
     
