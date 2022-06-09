575d236707c10927f3a56145944535392d636211
jfriisKU <xdr476@alumni.ku.dk>
2022-06-09 16:06:51 +0200

    Add interface for testing

---
+++ PetriNetEngine/PetriNetEngine/Application/IModelPetriNetService.cs
@@ -0,0 +1,16 @@
+using PetriNetEngine.Domain.Services;
+using SEA_Models.PetriNet;
+
+namespace PetriNetEngine.Application;
+
+public interface IModelPetriNetService
+{
+
+    List<PetriNet> GetAll();
+
+    PetriNet? GetPetriNet(int id);
+
+    PetriNet CreateNetPetriNet(PetriNet petriNetDto);
+
+    bool DeletePetriNet(int id);
+}
