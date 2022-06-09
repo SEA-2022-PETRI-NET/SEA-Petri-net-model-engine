052f0e3b4150551f79b5e44995786bd02b1a95b7
jfriisKU <xdr476@alumni.ku.dk>
2022-06-09 16:26:44 +0200

    Good changes

--- PetriNetEngine/PetriNetEngine.Tests/IntegrationTests/ModelPetriNetTest.cs
+++ PetriNetEngine/PetriNetEngine.Tests/IntegrationTests/ModelPetriNetTest.cs
@@ -7,10 +7,10 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using System;
 namespace PetriNetEngine.Tests;
 
-public class ApiTest
+public class ModelPetriNetTest
 {
     private readonly ITestOutputHelper _testOutputHelper;
-    public ApiTest(ITestOutputHelper testOutputHelper)
+    public ModelPetriNetTest(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
     }
@@ -31,43 +31,10 @@ public class ApiTest
         await using var application = new WebApplicationFactory<Program>();
         var client = application.CreateClient();
 
-        var response = await client.GetAsync("api/v1/Model");
+        var response = await client.PostAsync("api/v1/Model", System.Net.Http.HttpContent);
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         
     }
     
 }
-
-
-public class ApiTest
-{
-    private readonly ITestOutputHelper _testOutputHelper;
-    public ApiTest(ITestOutputHelper testOutputHelper)
-    {
-        _testOutputHelper = testOutputHelper;
-    }
-    
-    [Fact]
-    public async Task GET_modelling()
-    {
-        await using var application = new WebApplicationFactory<Program>();
-        var client = application.CreateClient();
-
-        var response = await client.GetAsync("api/v1/Model");
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-    }
-    
-    [Fact]
-    public async Task Create_moddeling()
-    {
-        await using var application = new WebApplicationFactory<Program>();
-        var client = application.CreateClient();
-
-        var response = await client.GetAsync("api/v1/Model");
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        
-    }
-    
-}
-
 
