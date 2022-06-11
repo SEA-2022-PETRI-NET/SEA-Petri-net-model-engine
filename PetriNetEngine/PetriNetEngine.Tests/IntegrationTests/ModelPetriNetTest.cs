0f57e2b6776189b0a5483d35995c85a004af67d0
jfriisKU <43671714+jfriisKU@users.noreply.github.com>
2022-06-11 11:57:39 +0200

    Update ModelPetriNetTest.cs

--- PetriNetEngine/PetriNetEngine.Tests/IntegrationTests/ModelPetriNetTest.cs
+++ PetriNetEngine/PetriNetEngine.Tests/IntegrationTests/ModelPetriNetTest.cs
@@ -20,7 +20,7 @@ public class ModelPetriNetTest
     }
     
     [Fact]
-    public async Task GET_modelling()
+    public async Task GET_model()
     {
         await using var application = new WebApplicationFactory<Program>();
         var client = application.CreateClient();
@@ -30,25 +30,11 @@ public class ModelPetriNetTest
     }
     
     [Fact]
-    public async Task Create_modeling()
+    public async Task POST_model()
     {
         await using var application = new WebApplicationFactory<Program>();
         var client = application.CreateClient();
 
-        PetriNet p = new PetriNet();
-        p.Places = new List<Place>()
-        {
-            new Place()
-        };
-        p.Transitions = new List<Transition>()
-        {
-            new Transition()
-        };
-        p.Arcs = new List<Arc>()
-        {
-            new Arc()
-        };
-        
         StringContent httpContent = new StringContent("{    \"name\": \"P\",    \"time\": 0,    \"maxTokenId\": 0,    \"arcs\": [      {        \"sourceNode\": 0,        \"targetNode\": 1,        \"type\": 0      }    ],    \"places\": [      {        \"placeId\": 1,        \"name\": \"Place\",        \"maxAge\": 0,        \"isUrgent\": false,        \"numberOfTokens\": 2147483647,        \"tokens\": [          {            \"id\": 0,            \"tokenId\": 0,            \"name\": \"string\",            \"age\": 0          }        ]      }    ],    \"transitions\": [      {        \"transitionId\": 0,        \"name\": \"0\"      }    ]  }", System.Text.Encoding.UTF8, "application/json");
 
         var response = await client.PostAsync("api/v1/Model", httpContent);
