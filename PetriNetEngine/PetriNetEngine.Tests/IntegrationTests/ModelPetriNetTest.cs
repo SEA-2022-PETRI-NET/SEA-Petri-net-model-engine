52897bb75649f7df470949fffd9d6b9f37834ae3
jfriisKU <43671714+jfriisKU@users.noreply.github.com>
2022-06-15 20:29:31 +0200

    Update ModelPetriNetTest.cs

--- PetriNetEngine/PetriNetEngine.Tests/IntegrationTests/ModelPetriNetTest.cs
+++ PetriNetEngine/PetriNetEngine.Tests/IntegrationTests/ModelPetriNetTest.cs
@@ -1,15 +1,11 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 using Xunit.Abstractions;
-using System.Threading.Tasks;
-using System.Net;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Mvc.Testing;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using SEA_Models.PetriNet;
 
-namespace PetriNetEngine.Tests;
+namespace PetriNetEngine.Tests.IntegrationTests;
 
 public class ModelPetriNetTest
 {
@@ -39,6 +35,36 @@ public class ModelPetriNetTest
 
         var response = await client.PostAsync("api/v1/Model", httpContent);
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        
+    }
+    
+    [Fact]
+    public async Task GET_model_id()
+    {
+        await using var application = new WebApplicationFactory<Program>();
+        var client = application.CreateClient();
+
+        StringContent httpContent = new StringContent("{    \"name\": \"P\",    \"time\": 0,    \"maxTokenId\": 0,    \"arcs\": [      {        \"sourceNode\": 0,        \"targetNode\": 1,        \"type\": 0      }    ],    \"places\": [      {        \"placeId\": 1,        \"name\": \"Place\",        \"maxAge\": 0,        \"isUrgent\": false,        \"numberOfTokens\": 2147483647,        \"tokens\": [          {            \"id\": 0,            \"tokenId\": 0,            \"name\": \"string\",            \"age\": 0          }        ]      }    ],    \"transitions\": [      {        \"transitionId\": 0,        \"name\": \"0\"      }    ]  }", System.Text.Encoding.UTF8, "application/json");
+
+        var res = await client.PostAsync("api/v1/Model", httpContent);
+        var response = await client.GetAsync("api/v1/Model/1");
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var response1 = await client.GetAsync("api/v1/Model/2147483647");
+        Assert.Equal(HttpStatusCode.NotFound, response1.StatusCode);
+        
+    }
+    
+    [Fact]
+    public async Task Post_Validate()
+    {
+        await using var application = new WebApplicationFactory<Program>();
+        var client = application.CreateClient();
+
+        StringContent httpContent = new StringContent("{    \"name\": \"P\",    \"time\": 0,    \"maxTokenId\": 0,    \"arcs\": [      {        \"sourceNode\": 0,        \"targetNode\": 1,        \"type\": 0      }    ],    \"places\": [      {        \"placeId\": 1,        \"name\": \"Place\",        \"maxAge\": 0,        \"isUrgent\": false,        \"numberOfTokens\": 2147483647,        \"tokens\": [          {            \"id\": 0,            \"tokenId\": 0,            \"name\": \"string\",            \"age\": 0          }        ]      }    ],    \"transitions\": [      {        \"transitionId\": 0,        \"name\": \"0\"      }    ]  }", System.Text.Encoding.UTF8, "application/json");
+
+        var response = await client.PostAsync("api/v1/Model/validate-petri-net", httpContent);
+        _testOutputHelper.WriteLine(response.ToString());
+        Assert.Equal(HttpStatusCode.Accepted, response.StatusCode);
         
     }
     
